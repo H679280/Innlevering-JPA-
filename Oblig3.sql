aa70b5f407056e5944ff28c80248ee7a9db452d3
i184895 <184895@stud.hvl.no>
2025-03-27 15:51:00 +0100

    hei

--- Oblig3.sql
+++ Oblig3.sql
@@ -11,21 +11,17 @@ SET search_path TO Firma;
  
 
 CREATE TABLE Ansatt ( 
-
     ansatt_id SERIAL PRIMARY KEY, 
-
     brukernavn VARCHAR(10) UNIQUE NOT NULL, 
-
     fornavn VARCHAR(50) NOT NULL, 
-
     etternavn VARCHAR(50) NOT NULL, 
-
     ansettelsesdato DATE NOT NULL, 
-
     stilling VARCHAR(50) NOT NULL, 
-
-    manedslonn NUMERIC(10,2) NOT NULL 
-
+    manedslonn NUMERIC(10,2) NOT NULL,
+    avd_id INTEGER NOT NULL,
+    prosjekt 
+    
+    CONSTRAINT AnsattFK FOREIGN KEY (avd_id) REFERENCES Avdeling(avd_id)
     -- legger til kobling etter hvert--  
 
 ); 
@@ -35,12 +31,12 @@ CREATE TABLE Ansatt (
 --lager avdeling-- 
 
 CREATE TABLE Avdeling ( 
-
     avd_id  SERIAL PRIMARY KEY, 
-
     navn VARCHAR(100) NOT NULL, 
-
-    -- legger til sjef_id senere-- 
+    sjef_id INTEGER,
+    
+    CONSTRAINT AvdelingFK FOREIGN KEY (sjef_id) REFERENCES Ansatt(ansatt_id)
+    -- legger til ansatt_id senere-- 
 
 ); 
 
@@ -54,38 +50,25 @@ ALTER TABLE Ansatt ADD COLUMN avd_id INT NOT NULL; ALTER TABLE Ansatt ADD CONSTR
 
 -- legger til eksempelverdier-- 
 
-INSERT INTO Avdeling (navn) VALUES  
-
+INSERT INTO Avdeling (navn) 
+VALUES  
 ('IT'),  
-
 ('HR'),  
-
 ('Økonomi'); 
 
  
 
 INSERT INTO Ansatt (brukernavn, fornavn, etternavn, ansettelsesdato, stilling, manedslonn, avd_id) VALUES  
-
 ('VH', 'Veronika', 'Nordmo', '2022-01-15', 'Utvikler', 55000, 1),  
-
 ('IB', 'Idunn', 'Blindheim', '2021-06-01', 'Prosjektleder', 65000, 2),  
-
 ('AFK', 'Anna', 'Kaland', '2023-03-10', 'Tester', 50000, 1),  
-
 ('OJ', 'Ole', 'Jensen', '2019-05-23', 'Systemadministrator', 60000, 1),  
-
 ('LM', 'Lars', 'Mikkelsen', '2020-08-17', 'HR-konsulent', 52000, 2),  
-
 ('TS', 'Therese', 'Solberg', '2022-09-12', 'Rekrutteringsansvarlig', 53000, 2),  
-
 ('KA', 'Kari', 'Andersen', '2018-11-30', 'Regnskapsfører', 58000, 3),  
-
 ('PB', 'Petter', 'Berg', '2020-04-14', 'Økonomisjef', 75000, 3),  
-
 ('ML', 'Marius', 'Lunde', '2023-01-10', 'Supportmedarbeider', 47000, 1),  
-
 ('HH', 'Hanne', 'Haugen', '2021-07-21', 'Personalansvarlig', 62000, 2 
-
 ); 
 
  
@@ -93,9 +76,6 @@ INSERT INTO Ansatt (brukernavn, fornavn, etternavn, ansettelsesdato, stilling, m
 SELECT *FROM ansatt; 
 
 SELECT *FROM avdeling; 
-
- 
-
  
 
 --kobler avdeling til ansatt-- 
