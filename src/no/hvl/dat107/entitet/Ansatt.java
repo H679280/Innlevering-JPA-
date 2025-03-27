8e94ca49ca4d5f9d8a54749f91544f5a5db5f6f8
i184895 <184895@stud.hvl.no>
2025-03-27 16:56:14 +0100

    hei

--- src/no/hvl/dat107/entitet/Ansatt.java
+++ src/no/hvl/dat107/entitet/Ansatt.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Ansatt2")
+@Table(name = "Ansatt")
 
 public class Ansatt {
 	@Id
@@ -24,6 +26,11 @@ public class Ansatt {
 	private LocalDate ansettelsesdato;
 	private String stilling;
 	private double manedslonn;
+	
+	
+	@ManyToOne
+	@JoinColumn (name = "avd_id")
+	private Avdeling avdeling; 
 
 	
 	public int getAnsatt_id() {
--- src/no/hvl/dat107/entitet/Avdeling.java
+++ src/no/hvl/dat107/entitet/Avdeling.java
@@ -1,45 +1,74 @@
 package no.hvl.dat107.entitet;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Avdeling")
 
 public class Avdeling {
-
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int avd_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private int avd_id;
 
-	@Column(nullable = false, unique = true)
-	private String sjef_id;
-	private String navn;
-	
-	public int getAvdId() {
-		return avd_id;
-	}
+    @OneToMany(mappedBy = "avdeling", fetch = FetchType.EAGER)
+    private List<Ansatt> ansatte;
 
-	public String getSjefId() {
-		return sjef_id;
-	}
-	
-	public String getNavn() {
-		return navn; 
-	}
-	
-	public void setNavn(String navn) {
-		this.navn=navn; 
-	}
+    @OneToOne
+    @JoinColumn(name = "ansatt_id")
+    private Ansatt sjef; 
 
+    @Column(nullable = false)
+    private String navn;
+
+    // Gettere og settere
+    public int getAvdId() {
+        return avd_id;
+    }
+
+    public void setAvdId(int avd_id) {
+        this.avd_id = avd_id;
+    }
+
+    public Ansatt getSjef() {
+        return sjef;
+    }
+
+    public void setSjef(Ansatt sjef) {
+        this.sjef = sjef;
+    }
+
+    public List<Ansatt> getAnsatte() {
+        return ansatte;
+    }
+
+    public void setAnsatte(List<Ansatt> ansatte) {
+        this.ansatte = ansatte;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
 
 	@Override
 	public String toString() {
-		return "Avdeling [avd_id= " + avd_id + "sjef id= " + sjef_id + "navn= " + navn + "]";
+		return "Avdeling [avd_id= " + avd_id + "Sjefen´s ansattid= " + sjef.getAnsatt_id() + "navn= " + navn + "]";
 	}
 
 }
